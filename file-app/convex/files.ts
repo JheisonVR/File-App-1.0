bfe094bae2bf2a0c9fd8c4267040375940849001
JheisonVR <daviid.vargasr@gmail.com>
2024-08-09 21:50:02 -0500

    chore: Add Header component and Convex schema

--- file-app/convex/files.ts
+++ file-app/convex/files.ts
@@ -1,3 +1,34 @@
-import {mutation} from "./_generated/server"
+import { ConvexError, v } from "convex/values"
+import {mutation, query} from "./_generated/server"
 
-export const createFile = mutation
+export const createFile = mutation({
+    args:{
+        name: v.string(),
+    },
+    async handler(ctx, args){
+
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new ConvexError("User not authenticated")
+        }
+
+        await ctx.db.insert("file", {
+            name: args.name,
+
+        })
+    }
+})
+
+export const getFiles = query({
+    args: {},
+    async handler(ctx, args){
+
+        const identity = await ctx.auth.getUserIdentity();
+         if(!identity){
+            return []
+         }
+
+        return ctx.db.query("file").collect()
+    }
+})
